27e06a72ecd8d91046a0ed79eefb99f9565d52ef
Ben Manning <bmanning@me.com>
2015-11-09 13:28:23 -0800

    modify solutions

--- solution/server.js
+++ solution/server.js
@@ -1,14 +1,19 @@
+// SERVER SIDE JAVASCRIPT
+
 var express = require('express');
 var app = express();
+var hbs = require('hbs');
+
+
 
 app.set('view engine', 'hbs');
-
 app.use(express.static('public'));
 
-var myName = 'Web Development';
+// seed data
+var myName = "Ben";
 
-// server.js
-  var albums = [
+// seed data for the albums
+var myAlbums = [
     { title: 'Cupid Deluxe',
       artist: 'Blood Orange'
     },
@@ -17,24 +22,42 @@ var myName = 'Web Development';
     },
     { title: 'Fake History',
       artist: 'letlive.'
-  }]
+  }];
+// seed data for the taquerias
+  var taquerias = [
+      { name: "La Taqueria" },
+      { name: "El Farolito" },
+      { name: "Taqueria Cancun" }
+    ];
 
-app.get('/', function (req, res) {
-  res.render('index', {  name: myName, paintings: paintings });
 
-  Handlebars.registerHelper('list', function(context, options) {
-      var ret = "<ul>";
+// each helper method for hbs template
+hbs.registerHelper('each', function(context, options) {
+  var ret = "";
 
-      for(var i=0, j=context.length; i<j; i++) {
-        ret = ret + "<li>" + options.fn(context[i]) + "</li>";
-      }
-    return ret + "</ul>";
-  });
+  for(var i=0, j=context.length; i<j; i++) {
+    ret = ret + options.fn(context[i]);
+  }
+	return ret;
 });
 
 
-var server = app.listen(3000, function () {
-var port = server.address().port;
 
-  console.log('Example app listening on port %s', port);
+// get the home route and return "Hello World!"
+app.get('/', function (req, res) {
+	res.render('index', { name: myName });
+});
+
+app.get('/albums', function(req, res) {
+	res.render('albums', {albums: myAlbums});
+});
+
+app.get('/api/taquerias', function (req, res) {
+	res.json(taquerias);
+});
+
+
+// in production use the production port, otherwise use 3000 (for development
+var server = app.listen(process.env.PORT || 3000, function () {
+	console.log("Example app listening at http://localhost:3000");
 });
