54b55e1ec6ca9cec78657919bd61312cb5d3c13c
tgaff <tgaff@alumni.nd.edu>
2015-11-28 18:27:25 -0800

    render albums to page

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,28 @@ console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
 
-  // code in here
+
+  $.ajax({
+         method: 'GET',
+         url: 'http://localhost:3000/api/albums',
+         success: function(data) { writeAlbums(data); },
+         error: function(e) {
+           console.log('uh oh');
+           $('#albumTarget').text('Failed to load albums, is the server working?');
+         }
+       });
+
 
 });
+
+
+
+// takes an array of albums and renders them as an unordered list
+function writeAlbums(albums) {
+  var outputHtml = '<ul>';
+  albums.forEach(function(album) {
+    outputHtml = outputHtml + "<li>" + album.artist + " -- " + album.title + "</li>";
+  });
+  outputHtml += '</ul>';
+  $('#albumTarget').html(outputHtml);
+}
