ec94771ca8bfd0acc7484f1e38ca2594fb55857b
tgaff <tgaff@alumni.nd.edu>
2015-11-28 18:00:40 -0800

    add comments to the server.js file

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 // server.js
 // SERVER SIDE JAVASCRIPT
 
+//require express in our app
 var express = require('express');
+// generate a new express app and call it 'app'
 var app = express();
 
 // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
@@ -11,10 +13,13 @@ app.use(function(req, res, next) {
   next();
 });
 
+// define a root route: localhost:3000/
 app.get('/', function (req, res) {
+  // send the text 'Hello World!' when someone visits localhost:3000/
   res.send('Hello World!');
 });
 
+// listen for connections on port 3000 or a port set in process.env.PORT
 var server = app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening at http://localhost:3000/');
 });
