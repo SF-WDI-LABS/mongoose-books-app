ecc9827d9e38449e3a84ade5df32605537941d5b
tgaff <tgaff@alumni.nd.edu>
2015-12-01 12:10:36 -0800

    use views directory for html

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 // server.js
-// SERVER SIDE JAVASCRIPT
+// SERVER-SIDE JAVASCRIPT
 
 //require express in our app
 var express = require('express');
 // generate a new express app and call it 'app'
 var app = express();
+
+// serve static files in public
+app.use(express.static('public'));
+
+var albums = [
+  {
+    title: 'Cupid Deluxe',
+    artist: 'Blood Orange'
+  },
+  {
+    title: 'M3LL155X - EP',
+    artist: 'FKA twigs'
+  },
+  {
+    title: 'Fake History',
+    artist: 'letlive.'
+  }
+];
+
 
 // Allow CORS: we'll use this today to reduce security so we can more easily test our code in the browser.
 app.use(function(req, res, next) {
@@ -13,31 +32,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-// serve static files in public
-app.use(express.static('public'));
-
-
-// server.js
-var albums = [
-  { title: 'Cupid Deluxe',
-    artist: 'Blood Orange'
-  },
-  { title: 'M3LL155X - EP',
-    artist: 'FKA twigs'
-  },
-  { title: 'Fake History',
-    artist: 'letlive.'
-}];
-
-
-
-
-
-app.get('/api/albums', function(req,res) {
-  res.json(albums);
-});
-
-
 // define a root route: localhost:3000/
 app.get('/', function (req, res) {
   // send the text 'Hello World!' when someone visits localhost:3000/
@@ -45,10 +39,13 @@ app.get('/', function (req, res) {
   console.log('res = ', res);
 
   //res.send('Hello World!');
-  res.sendFile('index.html' , { root : __dirname});
+  res.sendFile('views/index.html' , { root : __dirname});
 });
 
-// listen for connections on port 3000 or a port set in process.env.PORT
-var server = app.listen(process.env.PORT || 3000, function () {
+app.get('/api/albums', function(req,res) {
+  res.json(albums);
+});
+
+app.listen(process.env.PORT || 3000, function () {
   console.log('Example app listening at http://localhost:3000/');
 });
