c790ef671f760c554df52858e72a3d40f231f438
Juliana <jlopker@gmail.com>
2016-03-29 18:03:50 -0700

    release date in camel case

--- seed.js
+++ seed.js
@@ -8,49 +8,49 @@ var books_list = [
   title: "To Kill a Mockingbird",
   author: "Harper Lee",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/to_kill_a_mockingbird.jpg",
-  release_date: "July 11, 1960"
+  releaseDate: "July 11, 1960"
   },
   {
   title: "The Great Gatsby",
   author: "F Scott Fitzgerald",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/great_gatsby.jpg",
-  release_date: "April 10, 1925"
+  releaseDate: "April 10, 1925"
   },
   {
   title: "Les Miserables",
   author: "Victor Hugo",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/les_miserables.jpg",
-  release_date: "Unknown 1862"
+  releaseDate: "Unknown 1862"
   },
   {
   title: "Around the World in 80 Days",
   author: "Jules Verne",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/around_the_world_in_80_days.jpg",
-  release_date: "January 30, 1873"
+  releaseDate: "January 30, 1873"
   },
   {
   title: "Lean In",
   author: "Sheryl Sandberg",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/lean_in.jpg",
-  release_date: "March 11, 2013"
+  releaseDate: "March 11, 2013"
   },
   {
   title: "The Four Hour Workweek",
   author: "Tim Ferriss",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-  release_date: "April 1, 2007"
+  releaseDate: "April 1, 2007"
   },
   {
   title: "Of Mice and Men",
   author: "John Steinbeck",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-  release_date: "Unknown 1937"
+  releaseDate: "Unknown 1937"
   },
   {
   title: "Romeo and Juliet",
   author: "William Shakespeare",
   image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-  release_date: "Unknown 1597"
+  releaseDate: "Unknown 1597"
   }
 ];
 
